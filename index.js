2a82d1153aac61a88cc4fec5881c8eadeb4fd140
Manuel Weidmann <manuel.weidmann@7p-group.com>
2018-01-31 10:37:59 +0100

    feature: Add support for German clients
    Also fixes callback/emit referencing.

--- index.js
+++ index.js
@@ -124,26 +124,65 @@ const preludeQuotes = [
   'Can there be more quotes? Yes.'
 ]
 
-var inspirationRequest = {
-  uri: 'https://inspiroquote.herokuapp.com/inspiration',
-  headers: {
-    'x-inspiroquote-apikey': INSPIROQUOTE_API_KEY
-  },
-  json: true
-}
+var self
+const heroku = function (language) {
+  // if (language === 'en') {
+  //   self.emit(':ask', preludeQuotes[Math.random() * (preludeQuotes.length)])
+  // }
+  // retrieve a random quote from InspiroBot
+  var inspirationRequest = {
+    uri: 'https://inspiroquote.herokuapp.com/inspiration',
+    qs: {
+      'language': language
+    },
+    headers: {
+      'x-inspiroquote-apikey': INSPIROQUOTE_API_KEY
+    },
+    json: true
+  }
 
-const handlers = {
+  Please(inspirationRequest)
+    .then(function (response) {
+      self.emit(':tell', response.quote)
+    })
+    .catch(function (err) {
+      console.log(err)
+      if (language === 'de') {
+        self.emit(':tell', 'Oh nein, Irgendetwas ist da schiefgelaufen.')
+      } else {
+        self.emit(':tell', 'Oh no, something went wrong.')
+      }
+    })
+}
+const germanHandlers = {
+  'LaunchRequest': function () {
+    self = this
+    heroku('de')
+  },
   'inspire': function () {
-    this.emit(':tell', preludeQuotes[Math.random() * (preludeQuotes.length)])
-    // retrieve a random quote from InspiroBot
-    Please(inspirationRequest)
-      .then(function (response) {
-        this.emit(':tell', response.quote)
-      })
-      .catch(function (err) {
-        console.log(err)
-        this.emit(':tell', 'Oh no, something went wrong.')
-      })
+    self = this
+    heroku('de')
+  },
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = this.t('HELP_MESSAGE')
+    const reprompt = this.t('HELP_MESSAGE')
+    this.emit(':ask', speechOutput, reprompt)
+  },
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'))
+  },
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'))
+  }
+}
+const englishHandlers = {
+  'LaunchRequest': function () {
+    self = this
+    heroku('en')
+  },
+  'inspire': function () {
+    self = this
+    heroku('en')
   },
   'AMAZON.HelpIntent': function () {
     const speechOutput = this.t('HELP_MESSAGE')
@@ -160,8 +199,17 @@ const handlers = {
 
 exports.handler = function (event, context) {
   const alexa = Alexa.handler(event, context)
-  alexa.APP_ID = APP_ID
-  alexa.registerHandlers(handlers)
+  alexa.appId = APP_ID
+
+  var locale = event.request.locale
+  if (locale === 'de-DE') {
+    alexa.registerHandlers(germanHandlers)
+  } else {
+    alexa.registerHandlers(englishHandlers)
+  }
+
+  console.log(event)
+
   alexa.execute()
 }
 
